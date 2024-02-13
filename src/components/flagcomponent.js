1eae0c9dee8ec01585fae52c04e4c9370d3719f7
B-kumaraswamy <kumaraswamy491@gmail.com>
2024-02-13 08:43:31 +0530

    first commit

---
+++ src/components/flagcomponent.js
@@ -0,0 +1,23 @@
+function FlagsComponent(props) {
+    const {details, onDelete} = props
+    const {imageUrl, country, language} = details;
+
+    const onDeleteCountry = () => {
+        onDelete(language)
+
+    }
+    return (
+
+        <li>
+            <img src = {imageUrl} width = "5%"/>
+            <div>
+                <h2>{country}</h2>
+                <label>{language}</label>
+                <button onClick = {onDeleteCountry}>X</button>
+            </div>
+        </li>
+
+    )
+}
+
+export default FlagsComponent;
